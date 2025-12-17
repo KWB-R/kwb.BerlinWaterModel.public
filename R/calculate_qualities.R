2446990b13792ff1d8a64c33e5c3a0b7fab71272
mrustl <michael.rustler@kompetenz-wasser.de>
2025-12-17 10:15:33 +0100

    Fix calculate_qualities() documentation

--- R/calculate_qualities.R
+++ R/calculate_qualities.R
@@ -1,19 +1,20 @@
 #' Calculate Qualities
 #'
 #' @param input_list input_list as retrieved by \code{\link{prepare_input}}
-#' @param flows flows as retrieved by xxxx
+#' @param flows tibble with (modelled) flows for the network (e.g. precomputed
+#'   upstream by the user)
 #' @param network tibble with water cycle flow network data, as retrieved by
-#' \code{{\link{prepare_network}}}
+#'   \code{\link{prepare_network}}
 #' @param config list with config as imported with \code{\link{config_read}}
 #' @param reverse_flow calculate reverse flow (default: FALSE)
 #' @param branchwise improved calculation workflow minimising unneeded section
-#' calculations (default: TRUE)
-#' @param max_section restrict number of calculated sections in case problem
-#' occcur. Provide a number <= number of sections to be calculated. If NULL, all
-#' sections will be calculated (default: NULL). Only used if reverse_flow = FALSE
+#'   calculations (default: TRUE)
+#' @param max_sections restrict number of calculated sections in case problems
+#'   occur. Provide a number <= number of sections to be calculated. If NULL, all
+#'   sections will be calculated (default: NULL). Only used if reverse_flow = FALSE
 #' @param debug print debug messages (default: TRUE)
 #'
-#' @return returns modelled flows in tibble format
+#' @return returns modelled qualities in tibble format
 #' @export
 #' @importFrom dplyr count pull
 #' @importFrom stringr str_starts
